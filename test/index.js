075594e50b8b0153a005adfc29314bbcfab6a1cf
River Grimm <river.grimm@gmail.com>
2015-03-19 09:24:38 -0700

    add tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,35 @@
-var subset = require('..');
+var isSubset = require('..');
 var assert = require('assert');
 
-describe('subset(obj, sub)', function () {
-    it('returns true if `sub` is a subset of `obj`', function () {
-        var obj = {foo: 'bar', baz: 'qux'};
-        var sub = {baz: 'qux'};
-        assert(subset(obj, sub) === true);
-    });
-    it('returns false if `sub` is not a subset of `obj`', function () {
-        var obj = {foo: 'bar', baz: 'qux'};
-        var sub = {baz: 'quux'};
-        assert(subset(obj, sub) === false);
-    });
+describe('subset(original, subset)', function () {
+  var original;
+  var subset;
+  it('returns true if `subset` is a subset of `original`', function () {
+    original = {foo: 'a', bar: 'b'};
+    subset = {foo: 'a'};
+    assert(isSubset(original, subset) === true);
+  });
+  it('returns false if `subset` is not a subset of `original`', function () {
+    original = {foo: 'a', bar: 'b'};
+    subset = {foo: 'b'};
+    assert(isSubset(original, subset) === false);
+  });
+  it('iterates arrays', function () {
+    original = [{foo: 'a'}, {bar: 'b'}];
+    subset = [{foo: 'a'}];
+    assert(isSubset(original, subset) === true);
+    original = [{foo: 'a'}, {bar: 'b'}];
+    subset = [{foo: 'b'}];
+    assert(isSubset(original, subset) === false);
+  });
+  it('uses strict equality by default', function () {
+    original = {a: '1'};
+    subset = {a: 1};
+    assert(isSubset(original, subset) === false);
+  });
+  it('uses lenient equality when strict is set to false', function () {
+    original = {a: '1'};
+    subset = {a: 1};
+    assert(isSubset(original, subset, false) === true);
+  });
 });
