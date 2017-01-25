128ebba99734106d236f5b51735f4035d5407079
River <kvnneff@gmail.com>
2017-01-25 14:40:11 -0800

    Remove examples

--- examples/users.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  {id:1,first_name:'Stephen',last_name:'Butler',email:'sbutler0@rambler.ru',country:'Chile',ip_address:'102.81.37.240'},
-  {id:2,first_name:'Amy',last_name:'Mccoy',email:'amccoy1@weebly.com',country:'Poland',ip_address:'75.238.2.14'},
-  {id:3,first_name:'Joan',last_name:'Meyer',email:'jmeyer2@posterous.com',country:'Chile',ip_address:'100.102.116.78'},
-  {id:4,first_name:'Philip',last_name:'Armstrong',email:'parmstrong3@cafepress.com',country:'Japan',ip_address:'181.223.180.233'},
-  {id:5,first_name:'Diana',last_name:'Garcia',email:'dgarcia4@princeton.edu',country:'Netherlands',ip_address:'195.145.119.139'}
-];
